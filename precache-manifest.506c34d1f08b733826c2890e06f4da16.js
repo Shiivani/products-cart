abf9844f3652ca66b566c10c5ef2f5bd1b2a71f8
shivani1996.divyani@gmail.com <shivani1996.divyani@gmail.com>
2019-10-07 00:03:24 +0530

    Updates

---
+++ precache-manifest.506c34d1f08b733826c2890e06f4da16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9a0ef22e487ee4c6413461957c0559",
+    "url": "/products-cart/index.html"
+  },
+  {
+    "revision": "859ba658e19bf8f08134",
+    "url": "/products-cart/static/css/2.0e2c2b5a.chunk.css"
+  },
+  {
+    "revision": "9be8ca33c0cfee9c16e4",
+    "url": "/products-cart/static/css/main.8ba600e9.chunk.css"
+  },
+  {
+    "revision": "859ba658e19bf8f08134",
+    "url": "/products-cart/static/js/2.0345714e.chunk.js"
+  },
+  {
+    "revision": "9be8ca33c0cfee9c16e4",
+    "url": "/products-cart/static/js/main.e7372f78.chunk.js"
+  },
+  {
+    "revision": "e613c3cc04e591084621",
+    "url": "/products-cart/static/js/runtime-main.aae2718e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/products-cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/products-cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/products-cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/products-cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/products-cart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c7721a56d2ded1b62c0c7a6cf52de1ee",
+    "url": "/products-cart/static/media/logo.c7721a56.svg"
+  }
+]);
